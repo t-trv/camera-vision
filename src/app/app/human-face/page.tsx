b94fa8909690d98bf60cb75f42219369b3185a61
trv <trhuvan@gmail.com>
2026-03-31 23:26:03 +0700

    update: human and face page

--- src/app/app/human-face/page.tsx
+++ src/app/app/human-face/page.tsx
@@ -1,9 +1,15 @@
 import Heading from '@/components/ui/Heading';
+import FaceList from './_components/FaceList';
+import ControlBtn from './_components/ControlBtn';
 
 export default function HumanFacePage() {
   return (
     <div className="p-4 space-y-4">
       <Heading>Nhận diện khuôn mặt</Heading>
+
+      <ControlBtn />
+
+      <FaceList />
     </div>
   );
 }
